250c84a8ea3e1073880dc9a97d2540b8dbc05f73
Abhay Bhardwaj <abhaysippy307@gmail.com>
2026-04-03 19:12:46 +0530

    manipulation of html

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-// console.log(document.body.children)
+/**
+ * Manipulation of html 
+ * innerHTML
+ * innerText
+ * textContent
+ */
 
-// let box = document.getElementsByClassName("box")
-// let box = document.getElementById("box2")//2nd used
-// let h1 = document.getElementsByTagName("h1")
-// let box = document.querySelector(".box") //1st used
-
-// css --> tailwindcss
-//babel 
-
+// const heading = document.querySelector("#heading")
 
 
-let heading1 = document.querySelectorAll(".heading1")
+// heading.innerHTML = "this is new heading"
+// heading.innerHTML = "<h2>this is h2 <span>this is span</span></h2>"
 
-heading1.forEach(elem=>{
-    // elem.style.color = "blue"
-})
+// console.log(heading.innerText)
+// console.log(heading.textContent)
+
+/**
+ * attributes : we can see attributes of any element
+ * getAttribute : we can see a value of particular attribute
+ * setAttribute : we can set a values of any attribute
+ */
+
+// const heading = document.querySelector("#heading")
+
+// heading.setAttribute("class" , "ram")
 
 
-// forEach --> kuch bhi nahi return krna undefined
-// map --> return new Array
+//creation of element through javascript
+
+/**
+ * append : add element at the last of an element
+ * prepend : add element at the start of an element
+ * before : add element at the start of an element
+ * after : add element at the start of an element
+ * insertAdjacentElement(position, elem) beforebegin, beforeend, afterbegin afterend
+ * remove() : to remove an element
+ */
+let box = document.querySelector(".box")
+let p = document.createElement("p")
+
+// box.innerHTML = "<p>this is p created through javascript</p>"
+
+p.innerText = "this is p created through javascript"
+
+box.append(p)
+// p.remove()
+
+// box.remove()
+
+
+
+
 
