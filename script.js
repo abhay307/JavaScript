a8f7414d7851e05a486e3b782d798825df059dca
Abhay Bhardwaj <abhaysippy307@gmail.com>
2026-04-13 18:49:02 +0530

    manipulation of css

--- script.js
+++ script.js
@@ -1,53 +1,30 @@
 /**
- * Manipulation of html 
- * innerHTML
- * innerText
- * textContent
+ * Manipulation of css --> js se css add krna 
+ * 1. using attribute method
+ * 2. using style
+ * 3. use cssText Property
+ * 4. using className property
+ * 5. using classList property : add(), remove(), toggle(), contains()
  */
 
-// const heading = document.querySelector("#heading")
 
 
-// heading.innerHTML = "this is new heading"
-// heading.innerHTML = "<h2>this is h2 <span>this is span</span></h2>"
-
-// console.log(heading.innerText)
-// console.log(heading.textContent)
-
-/**
- * attributes : we can see attributes of any element
- * getAttribute : we can see a value of particular attribute
- * setAttribute : we can set a values of any attribute
- */
-
-// const heading = document.querySelector("#heading")
-
-// heading.setAttribute("class" , "ram")
+// let h1 = document.querySelector("#h1")
+//using attribute
+// h1.setAttribute("style", "color:blue; background-color : pink;")
 
 
-//creation of element through javascript
-
-/**
- * append : add element at the last of an element
- * prepend : add element at the start of an element
- * before : add element at the start of an element
- * after : add element at the start of an element
- * insertAdjacentElement(position, elem) beforebegin, beforeend, afterbegin afterend
- * remove() : to remove an element
- */
-let box = document.querySelector(".box")
-let p = document.createElement("p")
-
-// box.innerHTML = "<p>this is p created through javascript</p>"
-
-p.innerText = "this is p created through javascript"
-
-box.append(p)
-// p.remove()
-
-// box.remove()
+//****using style */
+// h1.style.color = "red"
+// h1.style.margin = "10px"
 
 
+//css text****
+// h1.style.cssText = "background-color : red; color : white"
+
+let h1 = document.querySelector("#h1")
 
 
+// h1.className = "red"
 
+console.log(h1.classList.contains("blue"))
