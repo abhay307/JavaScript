8c05a9bcbfdf1cbed28e2fe441bebff3daa9f49b
Abhay Bhardwaj <abhaysippy307@gmail.com>
2026-04-13 19:57:48 +0530

    event listner

--- script.js
+++ script.js
@@ -1,72 +1,61 @@
+// import btnClick from "./functions.js"
+//Event handling 
+
 /**
- * Manipulation of css --> js se css add krna
- * 1. using attribute method
- * 2. using style
- * 3. use cssText Property
- * 4. using className property
- * 5. using classList property : add(), remove(), toggle(), contains()
+ * - event : event is something that happens in the browser like user clicking,  mouse moving 
+ * eg
+ * 1. mouse event - click , hover , double click , etc.
+ * 2. keyboard event - keypress, keyup , keydown etc
+ * 3. form event - input , submit , change etc
+ * 4. window envet - scroll, resize etc
+ * 
+ * //monitorEvents(window)
  */
 
-// let h1 = document.querySelector("#h1")
-//using attribute
-// h1.setAttribute("style", "color:blue; background-color : pink;")
-
-//****using style */
-// h1.style.color = "red"
-// h1.style.margin = "10px"
-
-//css text****
-// h1.style.cssText = "background-color : red; color : white"
-
-// let h1 = document.querySelector("#h1")
-
-// // h1.className = "red"
-
-// console.log(h1.classList.contains("blue"))
-
-//create a div tag in html and give it a class and some styling. Now create a new class in css and try to append this class to the div element
-
-// const box = document.querySelector(".box")
-
-// box.classList.add("box2")
-
-//create a simple website with theme change functinality
-let body = document.querySelector("body");
-
-const btn = document.querySelector("#btn");
-
-let a = 0;
-
-//1. ans
-// btn.addEventListener("click", function () {
-//   if (a === 0) {
-//     body.style.backgroundColor = "black";
-//     body.style.color = "white";
-//     a = 1
-//   }else{
-//      body.style.backgroundColor = "white";
-//     body.style.color = "black";
-//     a = 0
-//   }
-// });
+/**
+ * 3 ways to handle events -
+ * 1. inline method  --> on , small 
+ * 2. property method --> on 
+ * 3.listner method
+ */
 
 
-//3 ans
-// btn.addEventListener("click", function () {
-//   body.classList.toggle("dark")
-// });
+// function ram(){
+//     console.log("helo how are you")
+// }
+
+// const btn = document.querySelector("#btn")
+
+// btn.onclick = ()=>{
+//     console.log("btn is clicked in property method event")
+// }
+
+// btn.onclick = ()=>{
+//     console.log("btn is clicked twice")
+// }
+
+const btn = document.querySelector("#btn")
+
+// btn.addEventListener(event, function) //syntax - way of writing
+
+// btn.removeEventListener("click", function(){
+//     console.log("btn click first time")
+// })
+// btn.removeEventListener("click", function(){
+//     console.log("btn click second  time")
+// })
 
 
-//2nd ans
-// btn.addEventListener("click", function () {
-//   if (a === 0) {
-//    body.className = "dark"
-//    a = 1
-//   }else{
-//       body.className = "light"
-//     a = 0
-//   }
-// });
+ function btnClick(){
+    let p = document.createElement("p")
+    p.innerHTML = "this is p created in js"
+    p.style.backgroundColor = "black"
+    p.style.color = "white"
+    p.style.padding = "20px"
+    let body = document.body
+    body.append(p)
+}
 
 
-//event listners
+btn.addEventListener("click", btnClick)
+
