c6c8f6307682753c1e81ec451b217ee4fabe7e97
Abhay Bhardwaj <abhaysippy307@gmail.com>
2026-04-13 19:18:35 +0530

    manipualiton of css end

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * Manipulation of css --> js se css add krna 
+ * Manipulation of css --> js se css add krna
  * 1. using attribute method
  * 2. using style
  * 3. use cssText Property
@@ -7,24 +7,66 @@
  * 5. using classList property : add(), remove(), toggle(), contains()
  */
 
-
-
 // let h1 = document.querySelector("#h1")
 //using attribute
 // h1.setAttribute("style", "color:blue; background-color : pink;")
-
 
 //****using style */
 // h1.style.color = "red"
 // h1.style.margin = "10px"
 
-
 //css text****
 // h1.style.cssText = "background-color : red; color : white"
 
-let h1 = document.querySelector("#h1")
+// let h1 = document.querySelector("#h1")
+
+// // h1.className = "red"
+
+// console.log(h1.classList.contains("blue"))
+
+//create a div tag in html and give it a class and some styling. Now create a new class in css and try to append this class to the div element
+
+// const box = document.querySelector(".box")
+
+// box.classList.add("box2")
+
+//create a simple website with theme change functinality
+let body = document.querySelector("body");
+
+const btn = document.querySelector("#btn");
+
+let a = 0;
+
+//1. ans
+// btn.addEventListener("click", function () {
+//   if (a === 0) {
+//     body.style.backgroundColor = "black";
+//     body.style.color = "white";
+//     a = 1
+//   }else{
+//      body.style.backgroundColor = "white";
+//     body.style.color = "black";
+//     a = 0
+//   }
+// });
 
 
-// h1.className = "red"
+//3 ans
+// btn.addEventListener("click", function () {
+//   body.classList.toggle("dark")
+// });
 
-console.log(h1.classList.contains("blue"))
+
+//2nd ans
+// btn.addEventListener("click", function () {
+//   if (a === 0) {
+//    body.className = "dark"
+//    a = 1
+//   }else{
+//       body.className = "light"
+//     a = 0
+//   }
+// });
+
+
+//event listners
